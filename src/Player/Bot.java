916dfc236ef9980ee39f0c0bca900c056a02b5da
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-10-09 22:21:34 +0200

    Added logic for bot to prioritize placing marker on the longest sequence possible. More testing needed.
    Added comments to bot methods.

--- src/Player/Bot.java
+++ src/Player/Bot.java
@@ -3,6 +3,8 @@ package Player;
 import Game.Board;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
 public class Bot extends Player {
@@ -38,41 +40,44 @@ public class Bot extends Player {
         }
     }
 
- /*
- * Takes the best coordinates and stores in ArrayList. Checks each coordinate and places it if possible. In order of column -> row -> diagnal -> anti-diagonal.
- * */
+    /**
+     * Stores the coordinates with the best sequence length of each direction
+     * @param board The board being played on.
+     * @return Returns true if a smart placement is found.
+     */
     public boolean bestPlacementPositionFound(Board board) {
         int boardSize = board.getBoardSize();
         Object[][] grid = board.getGrid();
-        int[] columnCoordinates = new int[4];
-        int[] rowCoordinates = new int[4];
-        int[] diagonalCoordinates = new int[4];
-        int[] antiDiagonalCoordinates = new int[4];
+        int[] columnCoordinates = new int[5];
+        int[] rowCoordinates = new int[5];
+        int[] diagonalCoordinates = new int[5];
+        int[] antiDiagonalCoordinates = new int[5];
         ArrayList<int[]> allCoordinates = new ArrayList<>();
-        int bestInRow = 0;
+        int highestInSequence = 0;
         //Column
         for (int x = 0; x < boardSize; x++) {
-            int markerInRow = 0;
+            int inSequence = 0;
             for (int y = 0; y < boardSize; y++) {
                 if (grid[x][y] != this) {
-                    markerInRow = 0;
+                    inSequence = 0;
                 } else {
-                    markerInRow++;
-                    if (markerInRow > bestInRow) {
+                    inSequence++;
+                    if (inSequence >= highestInSequence) {
                         System.out.println("Checking: COLUMN");
-                        bestInRow = markerInRow;
+                        highestInSequence = inSequence;
                         //Coordinates for inserting above and below column
                         columnCoordinates[0] = x;
                         columnCoordinates[1] = y + 1;
                         columnCoordinates[2] = x;
-                        columnCoordinates[3] = y - bestInRow;
+                        columnCoordinates[3] = y - highestInSequence;
+                        columnCoordinates[4] = highestInSequence;
                         allCoordinates.add(columnCoordinates);
                     }
                 }
             }
         }
         //Row
-        bestInRow = 0;
+        highestInSequence = 0;
         for (int y = 0; y < boardSize; y++) {
             int markerInRow = 0;
             for (int x = 0; x < boardSize; x++) {
@@ -80,54 +85,57 @@ public class Bot extends Player {
                     markerInRow = 0;
                 } else {
                     markerInRow++;
-                    if (markerInRow > bestInRow) {
+                    if (markerInRow >= highestInSequence) {
                         System.out.println("Checking: ROW");
-                        bestInRow = markerInRow;
+                        highestInSequence = markerInRow;
                         //Coordinates for inserting left and right of row
                         rowCoordinates[0] = x + 1;
                         rowCoordinates[1] = y;
-                        rowCoordinates[2] = x - bestInRow;
+                        rowCoordinates[2] = x - highestInSequence;
                         rowCoordinates[3] = y;
+                        rowCoordinates[4] = highestInSequence;
                         allCoordinates.add(rowCoordinates);
                     }
                 }
             }
         }
         //Diagonal
-        bestInRow = 0;
+        highestInSequence = 0;
         int markerInRow = 0;
         for (int i = 0; i < boardSize; i++) {
             if (grid[i][i] != this) {
                 markerInRow = 0;
             } else {
                 markerInRow++;
-                if (markerInRow > bestInRow) {
+                if (markerInRow >= highestInSequence) {
                     System.out.println("Checking: DIAGONAL");
-                    bestInRow = markerInRow;
+                    highestInSequence = markerInRow;
                     //Coordinates for inserting upper right corner and lower left corner of diagonal
                     diagonalCoordinates[0] = i + 1;
                     diagonalCoordinates[1] = i + 1;
-                    diagonalCoordinates[2] = i - bestInRow;
-                    diagonalCoordinates[3] = i - bestInRow;
+                    diagonalCoordinates[2] = i - highestInSequence;
+                    diagonalCoordinates[3] = i - highestInSequence;
+                    diagonalCoordinates[4] = highestInSequence;
                     allCoordinates.add(diagonalCoordinates);
                 }
             }
         }
-        bestInRow = 0;
+        highestInSequence = 0;
         markerInRow = 0;
         for (int i = 0; i < boardSize; i++)
             if (grid[i][boardSize - 1 - i] != this) {
                 markerInRow = 0;
             } else {
                 markerInRow++;
-                if (markerInRow > bestInRow) {
+                if (markerInRow >= highestInSequence) {
                     System.out.println("Checking: ANTI-DIAGONAL");
-                    bestInRow = markerInRow;
+                    highestInSequence = markerInRow;
                     //Coordinates for inserting upper left corner and lower right corner of diagonal
                     antiDiagonalCoordinates[0] = i - 1;
                     antiDiagonalCoordinates[1] = i + 1;
-                    antiDiagonalCoordinates[2] = i + bestInRow;
-                    antiDiagonalCoordinates[3] = i + bestInRow;
+                    antiDiagonalCoordinates[2] = i + highestInSequence;
+                    antiDiagonalCoordinates[3] = i + highestInSequence;
+                    antiDiagonalCoordinates[4] = highestInSequence;
                     allCoordinates.add(antiDiagonalCoordinates);
                 }
             }
@@ -140,8 +148,16 @@ public class Bot extends Player {
         }
     }
 
+    /**
+     * Sorts the coordinates passed to it by sequence length (index 4). Tries the coordinate with the highest sequence first.
+     * @param grid The grid being played on.
+     * @param coordinates A list of potential coordinates to try.
+     * @return Returns true if any of the given coordinates can be placed on the board.
+     */
     private boolean tryPlaceMarker(Object[][] grid, ArrayList<int[]> coordinates) {
         int index = 0;
+        coordinates.sort(Comparator.comparingInt(arr -> arr[4]));
+        Collections.reverse(coordinates);
         for (int[] coordinate : coordinates) {
             index++;
             System.out.println("TRY: " + index);
@@ -149,7 +165,6 @@ public class Bot extends Player {
             int y1 = coordinate[1];
             int x2 = coordinate[2];
             int y2 = coordinate[3];
-            //TODO Debug the try/catches, rarely seems to go to the second one.
             try {
                 if (grid[x1][y1] == null) {
                     grid[coordinate[0]][coordinate[1]] = this;
